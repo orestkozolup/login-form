c420a802ef6fdebede3aa29644780f3061580031
orestkozolup <53940769+orestkozolup@users.noreply.github.com>
2024-08-23 20:08:05 +0200

    Formik branch (#1)

--- app/components/auth-form/index.tsx
+++ app/components/auth-form/index.tsx
@@ -1,19 +1,67 @@
 "use client";
 
-import styles from './styles.module.css';
-
-import Input from '../input';
-import Button from '../button';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { validateEmail, validatePassword } from './utils';
 
 const AuthForm = () => {
+  const initialValues = {
+    email: '',
+    password: '',
+  };
+
+  const handleSubmit = (values: any) => {
+    console.log('Form Data:', values);
+  };
+
   return (
-    <div className={styles.container}>
-      <h2 className={styles.header}>Sign Up</h2>
-      <Input placeholder="Enter your email" />
-      <Input placeholder="Create your password" />
-      <Button label="Sign Up" onClick={() => console.log('HERE1')} />
-    </div>
-  )
-}
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validateOnBlur={true}
+      validateOnChange={false}
+    >
+      {({ handleChange, handleBlur, setFieldValue, errors, touched }) => (
+        <Form className="container">
+          <h2 className="header">Sign Up</h2>
+
+          {/* Email Field */}
+          <div>
+            <Field
+              name="email"
+              placeholder="Enter your email"
+              className="input"
+              onBlur={(e: any) => {
+                const { value } = e.target;
+                handleBlur(e);
+                const error = validateEmail(value);
+                setFieldValue('email', value, !error);
+              }}
+            />
+            {touched.email && errors.email && <div className="error">{errors.email}</div>}
+          </div>
+
+          {/* Password Field */}
+          <div>
+            <Field
+              name="password"
+              type="password"
+              placeholder="Create your password"
+              className="input"
+              onChange={(e: any) => {
+                const { value } = e.target;
+                handleChange(e);
+                const error = validatePassword(value);
+                setFieldValue('password', value, !error);
+              }}
+            />
+            {touched.password && errors.password && <div className="error">{errors.password}</div>}
+          </div>
+
+          <button type="submit">Sign Up</button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
 
 export default AuthForm;
